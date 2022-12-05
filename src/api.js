530662adb1292b2039a934ed5799261d6806a3ef
Artur <vanivskyi.artur@thinkful.com>
2022-12-04 21:39:33 -0500

    working fetch

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'de7672e099mshc255d70a64a449bp1ae90djsn43d48726e2aa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ea147b09139e14d560d0eed53114b6bb"
